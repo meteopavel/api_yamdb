2eda901ce0c8e66cd353fda41868bbca98e3391a
Adel Khakimov <harso95@gmail.com>
2023-09-26 10:27:42 +0300

    create category model

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=256,
+        unique=True,
+        verbose_name='Название категории'
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Slug категории'
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
